41a32411b5350c15ec2a7f570f1946902cb8d6c4
Lukaei <kyle@erdelyan.com>
2022-02-23 05:37:43 -0500

    Added Dowsing Rod

--- src/main/java/net/lukaei/tutorialmod/item/ModItems.java
+++ src/main/java/net/lukaei/tutorialmod/item/ModItems.java
@@ -2,6 +2,7 @@ package net.lukaei.tutorialmod.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.lukaei.tutorialmod.TutorialMod;
+import net.lukaei.tutorialmod.item.custom.DowsingRodItem;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -13,6 +14,8 @@ public class ModItems {
     public static final Item MYTHRIL_INGOT = registerItem("mythril_ingot", new Item(new FabricItemSettings().group(ModItemGroup.MYTHRIL)));
     public static final Item MYTHRIL_NUGGET = registerItem("mythril_nugget", new Item(new FabricItemSettings().group(ModItemGroup.MYTHRIL)));
     public static final Item RAW_MYTHRIL = registerItem("raw_mythril", new Item(new FabricItemSettings().group(ModItemGroup.MYTHRIL)));
+
+    public static final Item DOWSING_ROD = registerItem("dowsing_rod", new DowsingRodItem(new FabricItemSettings().group(ModItemGroup.MYTHRIL)));
 
     // Registers the ModItems
     private static Item registerItem(String name, Item item) {
