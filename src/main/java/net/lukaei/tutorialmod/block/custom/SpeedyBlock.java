f88e8cbba04aed93a90f0dfb52389d19bb40aa94
Lukaei <kyle@erdelyan.com>
2022-02-23 19:37:38 -0500

    Added Dowsing Rod

---
+++ src/main/java/net/lukaei/tutorialmod/block/custom/SpeedyBlock.java
@@ -0,0 +1,11 @@
+package net.lukaei.tutorialmod.block.custom;
+
+import net.minecraft.block.Block;
+
+public class SpeedyBlock extends Block {
+
+    public SpeedyBlock(Settings settings) {
+        super(settings);
+    }
+
+}
